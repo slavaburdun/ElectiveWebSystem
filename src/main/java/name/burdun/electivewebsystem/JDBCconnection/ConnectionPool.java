f5a884992a117ab34f8b6315c00192ffa94dbc87
Burdun Viacheslav <burdun.viacheslav@gmail.com>
2014-09-26 17:31:48 +0300

    Commit folder "JdbcConnection", because there are some problems with it in last commit

---
+++ src/main/java/name/burdun/electivewebsystem/JDBCconnection/ConnectionPool.java
@@ -0,0 +1,35 @@
+package name.burdun.electivewebsystem.JdbcConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ * Created by burdun on 26.09.2014.
+ */
+public class ConnectionPool {
+    private static final String DATASOURCE_NAME = "jdbc/elective";
+    private static DataSource dataSource;
+
+    static {
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+            dataSource = (DataSource) envContext.lookup(DATASOURCE_NAME);
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ConnectionPool() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        return connection;
+    }
+
+}
---
+++ src/main/java/name/burdun/electivewebsystem/JDBCconnection/WrapperConnector.java
@@ -0,0 +1,71 @@
+package name.burdun.electivewebsystem.JdbcConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * Created by burdun on 26.09.2014.
+ */
+public class WrapperConnector {
+
+    private Connection connection;
+
+    /* Use this for Tomcat
+    public WrapperConnector() {
+        try {
+            connection = ConnectionPool.getConnection();
+        } catch (SQLException e) {
+            System.err.println("not obtained connection " + e);
+        }
+    }
+    */
+
+    public WrapperConnector() {
+        try {
+            ResourceBundle resource = ResourceBundle.getBundle("database");
+            String url = resource.getString("db.url");
+            String user = resource.getString("db.user");
+            String pass = resource.getString("db.password");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (MissingResourceException e) {
+            System.err.println("properties file is missing " + e);
+        } catch (SQLException e) {
+            System.err.println("not obtained connection " + e);
+        }
+    }
+
+    public Statement getStatement() throws SQLException {
+        if (connection != null) {
+            Statement statement = connection.createStatement();
+            if (statement != null) {
+                return statement;
+            }
+        }
+        throw new SQLException("connection or statement is null");
+    }
+
+    public void closeStatement(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                System.err.println("statement is null " + e);
+            }
+        }
+    }
+
+    public void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.err.println(" wrong connection" + e);
+            }
+        }
+    }
+// другие необходимые делегированные методы интерфейса Connection
+}
